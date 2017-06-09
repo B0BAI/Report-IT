47db4d727193e5f6a13567cc4d5525717cf57998
Bobai Kato <bobaikato@gmail.com>
2017-06-09 13:57:10 +0100

    Implemented regular user sign up form.

--- app/src/main/java/me/bobaikato/app/report/Signup.java
+++ app/src/main/java/me/bobaikato/app/report/Signup.java
@@ -5,21 +5,35 @@ package me.bobaikato.app.report;
  * Twitter, Instagram, Github, GitLab: @BobaiKato
  */
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import static me.bobaikato.app.report.Permissions.checkNetwork;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Signup extends AppCompatActivity {
     EditText email, ppsnumber, password, username;
     TextView login_msg, signup, login;
+    private String email_val, ppsnumber_val, password_val, username_val;
 
-    String email_val, ppsnumber_val, password_val, username_val;
+    OkHttpClient client = new OkHttpClient();
+    private static final String URL = "https://www.report.lastdaysmusic.com/user/signup.php";
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +50,8 @@ public class Signup extends AppCompatActivity {
         login_msg = (TextView) findViewById(R.id.user_login);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
-        email = (EditText) findViewById(R.id.email);
+        email = (EditText) findViewById(R.id.email_add);
         ppsnumber = (EditText) findViewById(R.id.ppsno);
-
-
-        /*Values*/
-        username_val = username.getText().toString().trim();
-        password_val = password.getText().toString();
-        email_val = email.getText().toString().trim();
-        ppsnumber_val = ppsnumber.getText().toString().trim();
 
 
         ppsnumber.setTypeface(custom_font);
@@ -53,29 +60,94 @@ public class Signup extends AppCompatActivity {
         login_msg.setTypeface(custom_font);
         username.setTypeface(custom_font);
         email.setTypeface(custom_font);
+
+          /*Logging text click listener*/
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (!username_val.isEmpty() || username_val.length() != 0 || !username_val.equals("") || username_val != null) {
-
-                } else {
-
-                }
-
-
                 Intent it = new Intent(Signup.this, Login.class);
                 startActivity(it);
             }
         });
-             /*Check Permissions Connection*/
-        checkNetwork(findViewById(android.R.id.content), Signup.this);
+
+        /*Signup Onclick listener*/
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*Values*/
+                username_val = username.getText().toString().trim();
+                password_val = password.getText().toString();
+                email_val = email.getText().toString().trim();
+                ppsnumber_val = ppsnumber.getText().toString().trim();
+
+                //Toast.makeText(getApplicationContext(), "SIGNUP Clicked", Toast.LENGTH_SHORT).show();
+                if (email_val.isEmpty() || email_val.length() == 0 || email_val.equals("") || email_val == null) {
+                    Snackbar.make(v, "Email is invalid ", Snackbar.LENGTH_LONG).show();
+                } else if (username_val.isEmpty() || username_val.length() == 0 || username_val.equals("") || username_val == null) {
+                    Snackbar.make(v, "Username is invalid", Snackbar.LENGTH_LONG).show();
+                } else if (password_val.isEmpty() || password_val.length() == 0 || password_val.equals("") || password_val == null) {
+                    Snackbar.make(v, "Password is invalid", Snackbar.LENGTH_LONG).show();
+                } else if (ppsnumber_val.isEmpty() || ppsnumber_val.length() == 0 || ppsnumber_val.equals("") || ppsnumber_val == null) {
+                    Snackbar.make(v, "PPS Number is invalid", Snackbar.LENGTH_LONG).show();
+                } else {
+
+                    progressDialog = new ProgressDialog(Signup.this,
+                            R.style.AppTheme_Dark_Dialog);
+                    progressDialog.setIndeterminate(true);
+                    progressDialog.setMessage("Authenticating...");
+                    progressDialog.show();
+
+                    new Action().execute();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-         /*Check Permissions Connection*/
-        checkNetwork(findViewById(android.R.id.content), Signup.this);
+    class Action extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            RequestBody formBody = new FormBody.Builder()
+                    .add("email", email_val)
+                    .add("username", username_val)
+                    .add("password", password_val)
+                    .add("ppsno", ppsnumber_val)
+                    .build();
+            Request request = new Request.Builder()
+                    .url(URL)
+                    .post(formBody)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    progressDialog.dismiss();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    //progressDialog.dismiss();
+                    try {
+                        String resp = response.body().string();
+//                    Log.v(TAG_REGISTER, resp);
+                        System.out.println(resp);
+                        if (response.isSuccessful()) {
+                        } else {
+
+                        }
+                    } catch (IOException e) {
+                        // Log.e(TAG_REGISTER, "Exception caught: ", e);
+                        System.out.println("Exception caught" + e.getMessage());
+                    }
+                }
+            });
+
+            return null;
+        }
     }
 }
